db3472454863d111eaff7ed7959a5e4e59a5f37c
Ian Sawala <iansawalasaidi@gmail.com>
2026-03-27 23:36:47 +0300

    JVM and Native Tests
    Signed-off-by: Ian Sawala <iansawalasaidi@gmail.com>

---
+++ reservation-service/src/test/java/org/acme/reservation/ReservationResourceTest.java
@@ -0,0 +1,36 @@
+package org.acme.reservation;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.net.URL;
+import java.time.LocalDate;
+
+import org.acme.reservation.reservation.Reservation;
+import org.acme.reservation.rest.ReservationResource;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@QuarkusTest
+public class ReservationResourceTest {
+
+  @TestHTTPEndpoint(ReservationResource.class)
+  @TestHTTPResource
+  URL reservationResource;
+
+  @Test
+  public void testREservationIds() {
+    Reservation reservation = new Reservation();
+    reservation.carId = 12345L;
+    reservation.startDay = LocalDate.parse("2025-03-20");
+    reservation.endDay = LocalDate.parse("2025-03-29");
+
+    RestAssured.given().contentType(ContentType.JSON)
+        .body(reservation).when().post(reservationResource)
+        .then().statusCode(200).body("id", notNullValue());
+  }
+}
